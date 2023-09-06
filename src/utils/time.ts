72d07eeae7a4ac212332ead400d43a5dc853a4af
Mythdev2 <dev2@mythdigital.co>
2023-09-06 19:16:44 +0700

    Fix DDD-348: Restaurant > Prep times need to use the currentMode of the restaurant

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,7 +1,7 @@
 function displayTimeFromMinutes(minutes: number) {
   if (minutes < 60) {
     return `${minutes} mins`;
-  } else if (minutes > 60 && minutes < 1440) {
+  } else if (minutes >= 60 && minutes < 1440) {
     const hrs = Math.trunc(minutes / 60);
     return `${hrs} hrs`;
   } else if (minutes >= 1440) {
@@ -10,4 +10,4 @@ function displayTimeFromMinutes(minutes: number) {
   }
 }
 
-export { displayTimeFromMinutes }
+export {displayTimeFromMinutes}
